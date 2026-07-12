e1418c62b522f231ed36bea32cd854c0146934a2
Jayaram Subramanian <jayaram.subramanian@jayveecons.com>
2026-07-12 11:09:17 -0500

    fixed frontend json errors

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Screw Conveyor Designer</title>
-  <link rel="icon" type="image/svg+xml" href="/icon.svg"/>
-  <script type="module" crossorigin src="/assets/index-yCvZ3I3O.js"></script>
-  <link rel="stylesheet" crossorigin href="/assets/index-CvmEK7Nz.css">
-</head>
-<body><div id="root"></div></body>
-</html>
+export default {}
